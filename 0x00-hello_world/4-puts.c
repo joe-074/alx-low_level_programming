8e742483e3ad2c3a40687c33439b8452350735a3
Youssef Ragab <youssefragab10@gmail.com>
2023-02-16 09:50:22 -0800

    First

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 /**
 *main - Entry point
 *Return: Always 0 (Success)
@@ -9,10 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 			return (0);
-}
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
