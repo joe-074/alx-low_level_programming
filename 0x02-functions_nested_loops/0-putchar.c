10ca81eada168fb6cf6c55bcdde549af8c24e466
Youssef Ragab <youssefragab10@gmail.com>
2023-02-22 16:37:24 -0800

    Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -18,22 +18,3 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
-
-
-1-alphabet.c
-
-#include "main.h"
-
-/**
- * print_alphabet - print all alphabet in lowercase
- */
-
-void print_alphabet(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
-}
